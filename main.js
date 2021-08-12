7d551c8930b8bb5e15c8cf11f66ae3cb3252565e
Amosi-04-sanga <70682160+Amosi-04-sanga@users.noreply.github.com>
2021-08-12 16:34:48 +0300

    main.js added

---
+++ main.js
@@ -0,0 +1,352 @@
+// products menu array.
+const productsMenu = [
+
+    {
+        img: "./IMAGES/bed-1.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/bed-2.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$19.20"
+    },
+    {
+        img: "./IMAGES/bed-3.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/bed-4.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$30.20"
+    },
+    {
+        img: "./IMAGES/bed-5.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$40.20"
+    },
+    {
+        img: "./IMAGES/bed-6.jpg",
+        heading: "this is heading",
+        name: "bed",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$24.20"
+    },
+    {
+        img: "./IMAGES/clock__01.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/clock__02.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/clock__03.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/clock__04.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/clock__05.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/clock__06.jpg",
+        heading: "this is heading",
+        name: "clock",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__01.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__02.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__03.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__04.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__05.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__06.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__07.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__08.png",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__09.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/computer__10.jpg",
+        heading: "this is heading",
+        name: "computer",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$20.20"
+    },
+    {
+        img: "./IMAGES/nigerian-1.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/nigerian-2.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/nigerian-3.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$15.00"
+    },
+    {
+        img: "./IMAGES/nigerian-4.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$13.00"
+    },
+    {
+        img: "./IMAGES/nigerian-5.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$12.00"
+    },
+    {
+        img: "./IMAGES/nigerian-6.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$11.00"
+    },
+    {
+        img: "./IMAGES/nigerian-7.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$12.50"
+    },
+    {
+        img: "./IMAGES/nigerian-8.jpg",
+        heading: "this is heading",
+        name: "clothes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes1.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes2.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes3.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes4.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes5.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$10.00"
+    },
+    {
+        img: "./IMAGES/shoes6.jpg",
+        heading: "this is heading",
+        name: "shoes",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet sapiente aspernatur odio voluptatem est quia quisquam blanditiis iusto, dolore animi at.",
+        price: "$17.00"
+    }
+
+]; 
+
+// retrive renderProducts function on DOMLoad
+window.addEventListener( "DOMContentLoaded", () => {
+
+    renderProducts(productsMenu)
+    renderMenuButtons()
+
+} )
+
+// DOM selection
+const section = document.querySelector('.section')
+const menuButtons = document.getElementById('menu-buttons')
+
+// render menu buttons.
+function renderMenuButtons() {
+
+    // get unique menu.
+    const names = productsMenu.reduce( (values, item) => {
+        if( !values.includes(item.name) ) {
+           values.push(item.name)
+        }
+        return values
+    }, ["all"] )
+
+    // create buttons.
+    let buttons = names.map( name => {
+        return `<button data-id=${name} class="menu__button">
+                     ${name}
+                </button>`
+    } )
+
+    
+    buttons = buttons.join('')
+    menuButtons.innerHTML = buttons   
+    
+    let btnsArray = document.querySelectorAll('.menu__button')
+    btnsArray.forEach( btn => {
+        btn.addEventListener( 'click', e => {
+             
+            const menu = e.target.dataset.id.toLowerCase()
+             
+            if( menu === 'all' ) {
+                renderProducts(productsMenu)
+            }
+
+            else {
+                let menuItems = productsMenu.filter( menuItem => {
+
+                     if( menu === menuItem.name ) {
+                         return menuItem
+                     }
+
+                } )
+
+                renderProducts(menuItems)
+            }
+
+        } )
+    } )
+    
+}
+
+
+
+// function to render products.
+function renderProducts(array) {
+    
+   let products = array.map( product => {
+      
+        return `<!-- menu section -->
+        <div class="section__product">
+          <div class="section__photo-wrapper">
+              <img src=${product.img} alt=${product.name} class="section__photo">
+          </div>
+          
+          <h3 class="section__heading">${product.heading}</h3>
+          <p class="section__text">${product.description}.</p>
+  
+          <div class="section__flex">
+              <a href="#" class="section__link section__add-to-cart">add to cart</a>
+              <a href="#" class="section__link section__price">${product.price}</a>
+          </div>
+        </div>
+        <!-- menu section END-->`;
+
+   } );
+
+   products = products.join('')
+
+   section.innerHTML = products
+
+}
+
+
+
